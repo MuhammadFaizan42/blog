bcc5f7d7ae72096a0719f79fce903cba556577c9
MuhammadFaizan42 <m.faizan42.mf@gmail.com>
2024-06-11 18:03:34 +0500

    footer

--- src/layouts/footer.jsx
+++ src/layouts/footer.jsx
@@ -5,21 +5,19 @@ import Col from 'react-bootstrap/Col';
 
 const Footer = () => {
   return (
-    <div className='Container bg-dark'>
-        <Row>
-            <Col> 
-            <h4>About us</h4>
-            <p className='footer_text ms-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam </p>
-             </Col>
-            <Col> <h4>Quick Link </h4> </Col>
-            <Col> <h4>Category </h4> </Col>
-            <Col>News Letter</Col>
+
+    <div className='bg-dark text-white'>
+        <Container>
+          <Row>
+            <Col> 1 </Col>
+            <Col> 2</Col>
+            <Col> 3</Col>
+            <Col> 4</Col>
         </Row>
-
-
+        </Container>
     </div>
+    
+
   )
 }
 
