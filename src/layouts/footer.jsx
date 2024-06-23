3400aa4377c246b9510d925ab206faf1fedb3296
MuhammadFaizan42 <m.faizan42.mf@gmail.com>
2024-06-23 23:25:56 +0500

    blog home page design

--- src/layouts/footer.jsx
+++ src/layouts/footer.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from '../assets/logo.png'
+
 
 const Footer = () => {
   return (
 
     <div className='bg-dark text-white'>
-        <Container>
+        <Container className=''>
           <Row>
             <Col> 
             <h6> About</h6>
@@ -19,21 +26,81 @@ const Footer = () => {
               <small className='footer_text'>880 123 456 789</small>
             </h6>
              </Col>
-            <Col> 
-            <ul className="navbar-nav m-auto">
-            <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <Col className=''>
+            <h6 className='text-white'>Quick Link</h6>
+            <ul class="list-unstyled">
+            <li>
+              <a href="#!" className="text-decoration-none footer_text">Home</a>
             </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-            </ul> 
+            <li>
+              <a href="#!" className="text-decoration-none footer_text">About</a>
+            </li>
+            <li>
+              <a href="#!" className="text-decoration-none footer_text footer_text">Blog</a>
+            </li>
+            <li>
+              <a href="#!" className="text-decoration-none footer_text">Archived</a>
+            </li>
+            <li>
+              <a href="#!" className="text-decoration-none footer_text">Author</a>
+            </li>
+            <li>
+              <a href="#!" className="text-decoration-none footer_text">Contact</a>
+            </li>
+          </ul>
             </Col>
-            <Col> 3 </Col>
-            <Col> 4 </Col>
+            <Col className=''> 
+            <h6 className='text-white'>Category</h6>
+            <ul class="list-unstyled">
+            <li>
+              <a href="#!" className="text-decoration-none footer_text">Lifestyle</a>
+            </li>
+            <li>
+              <a href="#!" className="text-decoration-none footer_text">Technology</a>
+            </li>
+            <li>
+              <a href="#!" className="text-decoration-none footer_text footer_text">Travel</a>
+            </li>
+            <li>
+              <a href="#!" className="text-decoration-none footer_text">Business</a>
+            </li>
+            <li>
+              <a href="#!" className="text-decoration-none footer_text">Economy</a>
+            </li>
+            <li>
+              <a href="#!" className="text-decoration-none footer_text">Sports</a>
+            </li>
+          </ul>
+            </Col>
+            <Col> 
+            <Card  className='bg-dark text-white' style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title className='footer_text'>Weekly Newsletter</Card.Title>
+                  <Form>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                      <Form.Label className='footer_text'>Get blog articles and offers via email</Form.Label>
+                      <Form.Control className='bg-dark text-white' type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                  </Form>
+                  <Button variant="primary" style={{ width: '16rem' }}>Subscribe</Button>
+                </Card.Body>
+            </Card>
+
+            </Col>
         </Row>
         <Row>
-        <Col> 2 </Col>
+        <Col> 
+        <Navbar expand="lg" className="navbar bg-dark border-bottom border-body" data-bs-theme="dark" >
+            <Navbar.Brand href="#home">
+            <img src={Logo} alt="" />     
+              </Navbar.Brand>
+              <Nav className="m-auto">
+                <Nav.Link href="#home">Terms of Use</Nav.Link>
+                <Nav.Link href="#link">Privacy Policy</Nav.Link>
+                <Nav.Link href="#link">Cookie Policy</Nav.Link>
+              </Nav>
+          </Navbar>
+        </Col>
         </Row>
         </Container>
               
--- src/pages/features/index.jsx
+++ src/pages/features/index.jsx
@@ -2,12 +2,202 @@ import React from 'react'
 import Slide from '../../assets/slide.png'
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Badge from 'react-bootstrap/Badge';
+import CardAvatar from '../../assets/Image.png';
+import Stack from 'react-bootstrap/Stack';
+import CardImg from '../../assets/Rectangle.png';
+import Card2 from '../../assets/card2.png';
+import Avatar2 from '../../assets/avatar2.png';
+import Card3 from '../../assets/card3.png';
+import Avatar3 from '../../assets/avatar3.png';
+import Card4 from '../../assets/card4.png';
+import Avatar4 from '../../assets/avatar4.png';
+import Card5 from '../../assets/card5.png';
+import Avatar5 from '../../assets/avatar5.png';
+import Card6 from '../../assets/card6.png';
+import Avatar6 from '../../assets/avatar6.png';
+import Card7 from '../../assets/card7.png';
+import Card8 from '../../assets/card8.png';
+import Card9 from '../../assets/card9.png';
+
 
 const HomePage = () => {
   return (
     <div className='bg-dark'>
       <Container>
       <Image src={Slide} fluid/>;
+      <Row className='mt-3'>
+        <Col className='mt-2'>
+              <Card  className='bg-dark text-white' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={CardImg} />
+                <Card.Body>
+                <Badge className='bg-dark badges_text' >Technology</Badge>
+                <Card.Text className='cards'>
+                The Impact of Technology on the Workplace: How Technology is Changing
+                </Card.Text>
+                <div className="mt-1 author">
+                <img src={CardAvatar} class="rounded-circle" alt="..." />
+                <Stack direction="horizontal" gap={3}>
+                <div className="p-2">Tracey Wilson</div>
+                <div className="p-2">August 20, 2022</div>
+                </Stack> 
+              </div>
+                </Card.Body>
+              </Card>
+        </Col>
+        <Col className='mt-2'>
+        <Card  className='bg-dark text-white' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={Card2} />
+                <Card.Body>
+                <Badge className='bg-dark badges_text' >Technology</Badge>
+                <Card.Text className='cards'>
+                The Impact of Technology on the Workplace: How Technology is Changing
+                </Card.Text>
+                <div className="mt-1 author">
+                <img src={Avatar2} class="rounded-circle" alt="..." />
+                <Stack direction="horizontal" gap={3}>
+                <div className="p-2">Tracey Wilson</div>
+                <div className="p-2">August 20, 2022</div>
+                </Stack> 
+              </div>
+                </Card.Body>
+              </Card>
+        </Col>
+        <Col className='mt-2'>
+        <Card  className='bg-dark text-white' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={Card3} />
+                <Card.Body>
+                <Badge className='bg-dark badges_text' >Technology</Badge>
+                <Card.Text className='cards'>
+                The Impact of Technology on the Workplace: How Technology is Changing
+                </Card.Text>
+                <div className="mt-1 author">
+                <img src={Avatar3} class="rounded-circle" alt="..." />
+                <Stack direction="horizontal" gap={3}>
+                <div className="p-2">Tracey Wilson</div>
+                <div className="p-2">August 20, 2022</div>
+                </Stack> 
+              </div>
+                </Card.Body>
+              </Card>
+        </Col>
+      </Row>
+      <Row className='mt-3'>
+        <Col className='mt-2'>
+        <Card  className='bg-dark text-white' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={Card4} />
+                <Card.Body>
+                <Badge className='bg-dark badges_text' >Technology</Badge>
+                <Card.Text className='cards'>
+                The Impact of Technology on the Workplace: How Technology is Changing
+                </Card.Text>
+                <div className="mt-1 author">
+                <img src={Avatar4} class="rounded-circle" alt="..." />
+                <Stack direction="horizontal" gap={3}>
+                <div className="p-2">Tracey Wilson</div>
+                <div className="p-2">August 20, 2022</div>
+                </Stack> 
+              </div>
+                </Card.Body>
+              </Card>
+        </Col>
+        <Col className='mt-2'>
+        <Card  className='bg-dark text-white' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={Card5} />
+                <Card.Body>
+                <Badge className='bg-dark badges_text' >Technology</Badge>
+                <Card.Text className='cards'>
+                The Impact of Technology on the Workplace: How Technology is Changing
+                </Card.Text>
+                <div className="mt-1 author">
+                <img src={Avatar5} class="rounded-circle" alt="..." />
+                <Stack direction="horizontal" gap={3}>
+                <div className="p-2">Tracey Wilson</div>
+                <div className="p-2">August 20, 2022</div>
+                </Stack> 
+              </div>
+                </Card.Body>
+              </Card>
+        </Col>
+        <Col className='mt-2'>
+        <Card  className='bg-dark text-white' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={Card6} />
+                <Card.Body>
+                <Badge className='bg-dark badges_text' >Technology</Badge>
+                <Card.Text className='cards'>
+                The Impact of Technology on the Workplace: How Technology is Changing
+                </Card.Text>
+                <div className="mt-1 author">
+                <img src={Avatar6} class="rounded-circle" alt="..." />
+                <Stack direction="horizontal" gap={3}>
+                <div className="p-2">Tracey Wilson</div>
+                <div className="p-2">August 20, 2022</div>
+                </Stack> 
+              </div>
+                </Card.Body>
+              </Card>
+        </Col>
+      </Row>
+      <Row className='mt-3'>
+        <Col className='mt-2'>
+        <Card  className='bg-dark text-white' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={Card7} />
+                <Card.Body>
+                <Badge className='bg-dark badges_text' >Technology</Badge>
+                <Card.Text className='cards'>
+                The Impact of Technology on the Workplace: How Technology is Changing
+                </Card.Text>
+                <div className="mt-1 author">
+                <img src={Avatar2} class="rounded-circle" alt="..." />
+                <Stack direction="horizontal" gap={3}>
+                <div className="p-2">Tracey Wilson</div>
+                <div className="p-2">August 20, 2022</div>
+                </Stack> 
+              </div>
+                </Card.Body>
+              </Card>
+        </Col>
+        <Col className='mt-2'>
+        <Card  className='bg-dark text-white' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={Card8} />
+                <Card.Body>
+                <Badge className='bg-dark badges_text' >Technology</Badge>
+                <Card.Text className='cards'>
+                The Impact of Technology on the Workplace: How Technology is Changing
+                </Card.Text>
+                <div className="mt-1 author">
+                <img src={Avatar3} class="rounded-circle" alt="..." />
+                <Stack direction="horizontal" gap={3}>
+                <div className="p-2">Tracey Wilson</div>
+                <div className="p-2">August 20, 2022</div>
+                </Stack> 
+              </div>
+                </Card.Body>
+              </Card>
+        </Col>
+        <Col className='mt-2'>
+        <Card  className='bg-dark text-white' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={Card9} />
+                <Card.Body>
+                <Badge className='bg-dark badges_text' >Technology</Badge>
+                <Card.Text className='cards'>
+                The Impact of Technology on the Workplace: How Technology is Changing
+                </Card.Text>
+                <div className="mt-1 author">
+                <img src={CardAvatar} class="rounded-circle" alt="..." />
+                <Stack direction="horizontal" gap={3}>
+                <div className="p-2">Tracey Wilson</div>
+                <div className="p-2">August 20, 2022</div>
+                </Stack> 
+              </div>
+                </Card.Body>
+              </Card>
+        </Col>
+      </Row>
       </Container>
     </div>
   )
